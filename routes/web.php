804a57529b78cb4fec9522f456870eb906dfe360
BlkDem <m_sp@mail.ru>
2022-08-10 14:07:57 +0700

    first table CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
-<?php
+ <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\DeviceTypeController;
+use Illuminate\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,74 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+})->name('login');
+
+Route::post('/', function (\Illuminate\Http\Request $request) {
+    $email = $request->post('email');
+    $password = $request->post('password');
+    //$password = Hash::make($password);
+    //dd($password);
+    $result = \Illuminate\Support\Facades\Auth::attempt([
+        'email' => $email,
+        'password' => $password
+    ]);
+    
+    if ($result) {
+        return redirect(route('home'));
+    }
+    return redirect(route('login'));
 });
+
+Route::get('/web2', function () {
+    return view('login');
+})->name('login');
+
+Route::post('/web2', function (\Illuminate\Http\Request $request) {
+    $email = $request->post('email');
+    $password = $request->post('password');
+    //$password = Hash::make($password);
+    //dd($password);
+    $result = \Illuminate\Support\Facades\Auth::guard('web2')->attempt([
+        'email' => $email,
+        'password' => $password
+    ]);
+    
+    if ($result) {
+        return redirect(route('home2'));
+    }
+    return redirect(route('login2'));
+});
+
+Route::get('/security-1', function () {
+    return view('security');
+})->middleware('auth:web2')->name('home');
+
+Route::get('/security-2', function () {
+    return view('security');
+})->middleware('auth')->name('home2');
+
+Route::get('/logout', function () {
+    \Illuminate\Support\Facades\Auth::guard('web')->logout();
+});
+
+Route::get('/logout2', function () {
+    \Illuminate\Support\Facades\Auth::guard('web2')->logout();
+});
+
+Route::get('/device_types', 'DeviceTypeController@index');
+
+Route::prefix('v1')
+    ->name('v1')
+    ->group(function() {
+        Route::get('/device_types', [DeviceTypeController::class], 'index')
+            ->name('device_types.index');
+        Route::get('/device_types/{device_type}', [DeviceTypeController::class], 'show')
+            ->name('device_types.show');
+        Route::post('/device_types', [DeviceTypeController::class], 'store')
+            ->name('device_types.store');
+        Route::put('/device_types/{device_type}', [DeviceTypeController::class], 'update')
+            ->name('device_types.update');
+        Route::delete('/device_types/{device_type}', [DeviceTypeController::class], 'destroy')
+            ->name('device_types.delete');        
+    });
