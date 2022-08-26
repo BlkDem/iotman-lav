935270c89e787e92c54159e55dcc9db4eb6dd98d
BlkDem <m_sp@mail.ru>
2022-08-26 17:23:38 +0700

    before update routines

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http;
 use App\Http\Controllers\RootController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-/*Route::get('/', function () {
-    return view('login');
-})->name('login');*/
-
-/*Route::get('/{any?}', [
-    function () {
-        return view('welcome');
-    }
-])->where('any', '.*');*/
-
-/*Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-//Route::get('/{any}', [RootController::class, 'index'])->where('any', '.*');
-Route::get('{any}', function () {
-    return view('app');
-})->where('any', '.*');
+Route::get('/', [RootController::class, 'index'])->name('home');
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-*/
-Route::get('/', [App\Http\Controllers\RootController::class, 'index'])->name('home');
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
