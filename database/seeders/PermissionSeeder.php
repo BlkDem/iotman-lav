3cf950bd4e481055031c9eac2f39c2b137173d71
BlkDem <m_sp@mail.ru>
2023-05-03 12:21:20 +0300

    roles and permissions

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Auth\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $manageUser = new Permission();
+        $manageUser->name = 'Manage users';
+        $manageUser->slug = 'manage-users';
+        $manageUser->save();
+        $createTasks = new Permission();
+        $createTasks->name = 'Create Tasks';
+        $createTasks->slug = 'create-tasks';
+        $createTasks->save();
+    }
+}
