15b7cec4a6d24ad9a4a2c40ffa6513aa438515ea
BlkDem <m_sp@mail.ru>
2023-06-27 17:15:38 +0300

    predis install

--- config/cache.php
+++ config/cache.php
@@ -15,8 +15,8 @@ return [
     |
     */
 
-    // 'default' => env('CACHE_DRIVER', 'file'),
-    'default' => env('CACHE_DRIVER', 'redis'),
+    'default' => env('CACHE_DRIVER', 'file'),
+    // 'default' => env('CACHE_DRIVER', 'redis'),
 
     /*
     |--------------------------------------------------------------------------
